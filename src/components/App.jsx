7919327f44b3294221e8959a6a42d43206b12c4a
Zuzanna Maciejczyk <126521920+Zjadlbyscos@users.noreply.github.com>
2023-10-21 21:54:35 +0200

    foo App to class App, add state , render Statistic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,26 @@
+import React, { Component } from 'react';
 import Header from './header/Header';
 import Buttons from './buttons/Buttons';
+import Statistics from './statistics/Statistic';
 
-export const App = () => {
-  return (
-    <>
-      <Header></Header>
-      <Buttons></Buttons>
-    </>
-  );
-};
+ class App extends Component{
+  state = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      };
+      render() {
+            const { good, neutral, bad } = this.state;
+        
+            return (
+              <div>
+                <Header />
+                <Buttons onClick={this.handleButtonClick} />
+                <Statistics good={good} neutral={neutral} bad={bad} />
+              </div>
+            );
+          }
+
+
+ }
+ export default App;
